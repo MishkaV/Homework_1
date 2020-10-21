a1cfae8d198a28784fbefecb62a19fb561b3ea38
Misha <mvorozhtsov@bk.ru>
2020-10-21 20:28:57 +0300

    try

---
+++ app/src/main/java/com/example/homework1/ListNumbers.kt
@@ -0,0 +1,21 @@
+package com.example.homework1
+
+class ListNumbers {
+    private var numbers :  ArrayList<String> = ArrayList()
+
+    fun init()
+    {
+        for(i in 1..100)
+            this.numbers.add("$i")
+    }
+    fun addNum()
+    {
+        this.numbers.add((this.numbers.last().toInt() + 1).toString())
+    }
+
+    fun getList() : ArrayList<String>
+    {
+        return  this.numbers
+    }
+
+}
